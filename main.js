01faf9a7be070c034a672261200c6f62ad15ba7d
Sigal Shalit Manor <sigal.shalit.manor@gmail.com>
2018-07-28 17:24:09 +0300

    complete the exercise

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+//coffee-shop
+
 var coffeeShop = {
   beans: 40,
 
@@ -8,9 +10,29 @@ var coffeeShop = {
     frenchPress: 12
   },
 
-  makeDrink: function (drinkType) {
-    // TODO: Finish this method
+  findDrink : function(drinkType) {
+    if (this.drinkRequirements[drinkType]) {
+      return this.drinkRequirements[drinkType];
+    }
+    alert("Sorry, we don't make " + drinkType);
+    return null;
+  },
+  
+  makeDrink: function(drinkType) {
+    let drink = this.findDrink(drinkType);
+
+    if (drink === null) {
+      return;
+    }
+
+    if (this.beans >= drink) {
+      this.beans -= drink;
+      console.log(this.beans);
+    } else {
+      alert("Sorry, we're all out of beans!");
+    }
   }
+ 
 }
 
 coffeeShop.makeDrink("latte");
@@ -18,3 +40,32 @@ coffeeShop.makeDrink("americano");
 coffeeShop.makeDrink("filtered");
 coffeeShop.makeDrink("doubleShot");
 coffeeShop.makeDrink("frenchPress");
+
+
+
+ /*beansCountForDrinkType: function(drinkType) {
+    if (this.beans >= this.findDrink(drinkType)) {
+      console.log(this.beans -= this.findDrink(drinkType));
+      return this.beans -= this.findDrink(drinkType);
+    } 
+    alert("Sorry, we're all out of beans!");
+    return null;
+    }
+  },*/
+
+  /*  makeDrink: function(drinkType) {
+      for (let key in this.drinkRequirements) {
+        if (drinkType === key) {
+          if (this.beans >= this.drinkRequirements[drinkType]) {
+            this.beans -= this.drinkRequirements[drinkType];
+            console.log(this.beans);
+            return; 
+          } else {
+            alert("Sorry, we're all out of beans!");
+            return;
+          } 
+        } 
+    }
+    alert("Sorry, we don't make " + drinkType);
+  }
+ */ 
